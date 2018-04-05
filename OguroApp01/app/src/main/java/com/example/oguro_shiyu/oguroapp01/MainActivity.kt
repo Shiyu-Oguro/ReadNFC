b02e6c7d2195b1b4e59752dad21a8658b68936ec
shiyuu09@gmail.com <shiyuu09@gmail.com>
2018-04-05 17:02:14 +0900

    Firebase Authentication Example in Kotlin のCreateAccountまで終了

---
+++ OguroApp01/app/src/main/java/com/example/oguro_shiyu/oguroapp01/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.oguro_shiyu.oguroapp01
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by oguro_shiyu on 2018/04/05.
+ */
+class MainActivity  : AppCompatActivity() {
+}
